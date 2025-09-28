63b1d401d1eed9720eaa2caf6b42aae79bf17a5d
Vladimir <vlodmerpoznyakov@gmail.com>
2025-09-28 10:30:58 +0300

    7 lab

--- myproject/settings.py
+++ myproject/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
-    'shop2',
 ]
 
 MIDDLEWARE = [
--- shop/views.py
+++ shop/views.py
@@ -1,6 +1,16 @@
+# shop/views.py
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 
 def index(request):
-    products = Product.objects.all()
+    
+    products = Product.objects.select_related("category").all()
     return render(request, "shop/index.html", {"products": products})
+
+def products_list(request):
+    products = Product.objects.select_related("category").all()
+    return render(request, "shop/products.html", {"products": products})
+
+def categories_list(request):
+    categories = Category.objects.all()
+    return render(request, "shop/categories.html", {"categories": categories})
